8b97f0eb7b211c6bba8a9d355f9147e591afb098
Sadiq Noor <--replace-all>
2020-12-20 15:07:21 +0400

    9) API calls with Axios

--- real-world-vue/src/views/EventList.vue
+++ real-world-vue/src/views/EventList.vue
@@ -1,14 +1,34 @@
 <template>
   <div>
     <h1>List Event</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event"></EventCard>
   </div>
 </template>
 <script>
 import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
+//import axios from "axios";
+
 export default {
   components: {
     EventCard
-  }
+  },
+  data() {
+    return {
+      events:[]
+    }
+  },
+  created () {
+    //axios.get("http://localhost:3000/events")
+
+    EventService.getEvents()
+      .then(response => {
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('there was an error:', error.response)
+      })
+  },
+
 };
 </script>
