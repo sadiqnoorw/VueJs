f27736c4e5f1b7ce4aa0d685b401e28fbb7fbcc8
Sadiq Abid <sadiq.pk33@gmail.com>
2021-04-24 02:50:19 +0400

    vuex mastering  mutations and action

--- mastering-vuex-videos-tutorial/src/store/index.js
+++ mastering-vuex-videos-tutorial/src/store/index.js
@@ -17,20 +17,65 @@ export default new Vuex.Store({
       'food',
       'community'
     ],
-    events: []
+    events: [],
+    eventsTotal: 0,
+    event: {}
   },
-  getters: {},
+  getters: {
+    getEventById: state => id => {
+      return state.events.find(event => event.id === id)
+    }
+  },
   mutations: {
     ADD_EVENT(state, event){
       state.events.push(event)
+    },
+    SET_EVENTS(state, events){
+      state.events = events
+    },
+    SET_EVENTS_TOTAL(state, eventsTotal){
+      state.eventsTotal = eventsTotal
+    },
+    SET_EVENT(state, event){
+      state.event = event
     }
   },
+
   actions: {
     createEvent({ commit }, event) {
       return EventService.postEvent(event).then(() => {
         commit('ADD_EVENT', event)
       })
+    },
+    fetchEvents( { commit }, {perPage, page}) {
+      EventService.getEvents(perPage, page)
+      .then((response) => {
+        commit(
+          'SET_EVENTS_TOTAL',
+          parseInt(response.headers['x-total-count'])
+        )
+        commit('SET_EVENTS', response.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      });
+    },
+
+    fetchEvent( {commit, getters}, id){
+      var event =  getters.getEventById(id)
+      if(event){
+        commit('SET_EVENT', event)
+      } else {
+        EventService.getEvent(id)
+          .then(response => {
+              commit('SET_EVENT', response.data)
+          }).
+          catch(error => {
+              console.log(error)
+          })
+      }
     }
+
   },
   modules: {},
   // state: {
--- mastering-vuex-videos-tutorial/src/views/EventList.vue
+++ mastering-vuex-videos-tutorial/src/views/EventList.vue
@@ -1,30 +1,39 @@
 <template>
   <div>
-    <h1>Listing Event</h1>
+    <h1>Listing {{ user.name }} {{ eventsTotal}}</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <template v-if="page !=1">
+      <router-link :to="{ name: 'event-list', query: { page: page - 1}}" rel="prev"> prev page</router-link> |
+    </template>
+    <template v-if="nextPage">
+      <router-link :to="{ name: 'event-list', query: { page: page + 1}}" rel="next"> next page</router-link>
+      </template>
   </div>
 </template>
 <script>
 import EventCard from "@/components/EventCard.vue";
-import EventService from "@/Services/EventServices.js";
-
+//import EventService from "@/Services/EventServices.js";
+import {mapState} from 'vuex'
 export default {
   components: {
     EventCard,
   },
-  data() {
-    return {
-      events: [],
-    };
-  },
   created() {
-    EventService.getEvents()
-      .then((response) => {
-        this.events = response.data;
+    this.$store.dispatch('fetchEvents', {
+      perPage: 3,
+      page: this.page
       })
-      .catch((error) => {
-        console.log(error);
-      });
+     // console.log(eventsTotal)
   },
+  computed: {
+    page(){
+      return parseInt(this.$route.query.page) || 1
+    },
+    nextPage(){
+      return this.eventsTotal > this.page * 3
+    },
+    ...mapState(['events', 'eventsTotal', 'user'])
+  }
+  
 };
 </script>
